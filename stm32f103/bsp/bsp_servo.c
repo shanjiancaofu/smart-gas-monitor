e775d9d110118abcd1b6f1146fc8de7e6d26e814
shanjiancaofu <528432910@qq.com>
2026-09-17 18:40:20 +0800

    refactor: unify servo PWM across targets

--- stm32f103/bsp/bsp_servo.c
+++ stm32f103/bsp/bsp_servo.c
@@ -1,69 +1,7 @@
 #include "bsp_servo.h"
 #include "tim.h"
 
-#ifndef USE_SOFT_I2C
-#define USE_SOFT_I2C 0
-#endif
-
-#if USE_SOFT_I2C
-/* The Proteus MOTOR-PWMSERVO in this project is configured for 1..2 ms. */
-#define SERVO_CLOSE_US 1000u
-#define SERVO_OPEN_US 2000u
-static volatile uint16_t requested_pulse_us = SERVO_CLOSE_US;
-
-bool bsp_servo_init(void)
-{
-    GPIO_InitTypeDef gpio = {0};
-
-    __HAL_TIM_DISABLE(&htim4);
-    __HAL_TIM_DISABLE_IT(&htim4, TIM_IT_UPDATE | TIM_IT_CC3);
-
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    gpio.Pin = GPIO_PIN_8;
-    gpio.Mode = GPIO_MODE_OUTPUT_PP;
-    gpio.Pull = GPIO_NOPULL;
-    gpio.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &gpio);
-
-    requested_pulse_us = SERVO_CLOSE_US;
-    __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, SERVO_CLOSE_US);
-    __HAL_TIM_SET_COUNTER(&htim4, 0u);
-    __HAL_TIM_CLEAR_FLAG(&htim4, TIM_FLAG_UPDATE | TIM_FLAG_CC3);
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
-
-    HAL_NVIC_SetPriority(TIM4_IRQn, 0u, 2u);
-    HAL_NVIC_EnableIRQ(TIM4_IRQn);
-    __HAL_TIM_ENABLE_IT(&htim4, TIM_IT_UPDATE | TIM_IT_CC3);
-    __HAL_TIM_ENABLE(&htim4);
-    return true;
-}
-
-void bsp_servo_set(bool open)
-{
-    requested_pulse_us = open ? SERVO_OPEN_US : SERVO_CLOSE_US;
-}
-
-/* Proteus does not reliably expose TIM4_CH3 through PB8 alternate function.
-   Keep TIM4's precise 1 MHz counter, but drive PB8 directly on update/compare. */
-void TIM4_IRQHandler(void)
-{
-    bool update = __HAL_TIM_GET_FLAG(&htim4, TIM_FLAG_UPDATE) != RESET &&
-                  __HAL_TIM_GET_IT_SOURCE(&htim4, TIM_IT_UPDATE) != RESET;
-    bool compare = __HAL_TIM_GET_FLAG(&htim4, TIM_FLAG_CC3) != RESET &&
-                   __HAL_TIM_GET_IT_SOURCE(&htim4, TIM_IT_CC3) != RESET;
-
-    if (compare) {
-        __HAL_TIM_CLEAR_IT(&htim4, TIM_IT_CC3);
-        GPIOB->BRR = GPIO_PIN_8;
-    }
-    if (update) {
-        __HAL_TIM_CLEAR_IT(&htim4, TIM_IT_UPDATE);
-        __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, requested_pulse_us);
-        GPIOB->BSRR = GPIO_PIN_8;
-    }
-}
-#else
-/* Physical servo full travel, matching the verified reference implementation. */
+/* One implementation for Proteus and hardware: PB8/TIM4_CH3, 50 Hz. */
 #define SERVO_CLOSE_US 500u
 #define SERVO_OPEN_US 2500u
 
@@ -81,4 +19,3 @@ void bsp_servo_set(bool open)
     __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3,
                           open ? SERVO_OPEN_US : SERVO_CLOSE_US);
 }
-#endif
